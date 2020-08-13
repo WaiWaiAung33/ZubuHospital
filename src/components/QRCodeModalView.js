5bf86888313a75896fd568d711aa429106f57bca
WaiWaiAung33 <nawtoo1997@gmail.com>
2020-08-13 10:16:42 -0800

    [WWA]

---
+++ src/components/QRCodeModalView.js
@@ -0,0 +1,162 @@
+import React from "react";
+import {
+  TouchableOpacity,
+  Modal,
+  View,
+  Dimensions,
+  StyleSheet,
+  Image,
+  Alert,
+} from "react-native";
+
+import { QRCode } from "react-native-custom-qr-codes";
+
+const { width } = Dimensions.get("window");
+export default class QRCodeModalView extends React.Component {
+  close() {
+    if (this.props.onClose) {
+      this.props.onClose();
+    }
+  }
+
+  render() {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={this.props.isOpen}
+        onRequestClose={() => {
+          this.close();
+        }}
+      >
+        {/* {this.props.userID ? ( */}
+        <View
+          style={[
+            styles.modalContainer,
+            { backgroundColor: "rgba(52, 52, 52, 0.4)" },
+          ]}
+        >
+          <View style={[styles.modalBody, { backgroundColor: "#fff" }]}>
+            <View style={{ flexDirection: "row" }}>
+              <View
+                style={{
+                  alignItems: "center",
+                  flex: 1,
+                }}
+              >
+                {/* <Image
+                  source={
+                    this.props.headerIcon
+                      ? this.props.headerIcon
+                      : require("@images/qrcode-msg.png")
+                  }
+                  style={styles.headerIcon}
+                /> */}
+              </View>
+
+              <TouchableOpacity
+                onPress={() => this.close()}
+                style={[styles.closeImg]}
+              >
+                <Image
+                  source={require("@images/cancel.png")}
+                  style={styles.closeIcon}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.qrCodeWrapper}>
+              {/* {this.props.userID ? ( */}
+              <QRCode
+                // content={this.props.userID}
+                codeStyle="square"
+                size={200}
+                // logo={require("@icons/logos/Logo.png")}
+                // linearGradient={["#00807E", "#3BC6C3"]}
+              />
+              {/* ) : null} */}
+            </View>
+          </View>
+        </View>
+        {/* ) : (
+          <View
+            style={[
+              styles.modalContainer,
+              { backgroundColor: "rgba(52, 52, 52,1)" }
+            ]}
+          >
+            <View style={styles.modalBody}>
+              <View style={{ flexDirection: "row" }}>
+                <View
+                  style={{
+                    alignItems: "center",
+                    flex: 1
+                  }}
+                ></View>
+              </View>
+              <View style={styles.qrCodeWrapper}>
+                <TouchableOpacity onPress={() => this.close()} activeOpacity={0.7}>
+                  <Image
+                    style={styles.image}
+                    source={{ uri: this.props.img }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        )} */}
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  modalBody: {
+    width: "100%",
+    height: null,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  headerIcon: {
+    width: 42,
+    height: 42,
+    marginTop: 10,
+  },
+  image: {
+    width: width - 40,
+    height: width,
+    resizeMode: "stretch",
+  },
+  qrCodeWrapper: {
+    padding: 20,
+    justifyContent: "center",
+    height: null,
+  },
+  qrCodeImg: {
+    width: width / 2,
+    height: width / 2,
+  },
+  closeImg: {
+    position: "absolute",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    right: 0,
+  },
+  closeImage: {
+    position: "absolute",
+    padding: 10,
+    right: 0,
+    top: 0,
+  },
+  closeIcon: {
+    width: 25,
+    height: 25,
+  },
+});
--- src/screens/account/Profile.js
+++ src/screens/account/Profile.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
-import { DrawerActions } from "react-navigation-drawer";
+import { QRCode } from "react-native-custom-qr-codes";
+
+//import components
+import QRCodeModalView from "@components/QRCodeModalView";
 
 //import components
 import Header from "@components/Header";
@@ -9,6 +19,9 @@ import Header from "@components/Header";
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOpenQRCodeModal: false,
+    };
   }
   _handleOnPress() {
     this.props.navigation.navigate("HDashboardNavigator");
@@ -21,20 +34,45 @@ export default class Profile extends React.Component {
           //   img={require("@images/threeline.png")}
           Onpress={() => this._handleOnPress()}
         />
-        <View style={styles.secondContainer}>
-          <Image
-            source={require("@images/people.jpg")}
-            style={{ width: 100, height: 100, borderRadius: 5 }}
-          />
-          <Text style={styles.text}>Dr. Maung Maung</Text>
-          <Text style={styles.text}>Doctor</Text>
-          <Text style={styles.text}>Department 1</Text>
-          <Text style={styles.text}>09 458518277</Text>
-          <Text style={styles.text}>7/khtthaka(N)111111</Text>
-          <Text style={[styles.text, { height: 80, textAlignVertical: "top" }]}>
-            Naypyitaw
-          </Text>
-        </View>
+        <ScrollView>
+        <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.qrcodeBox}
+            onPress={() => this.setState({ isOpenQRCodeModal: true })}
+          >
+            {/* {this.state.user_id ? ( */}
+            <QRCode
+              // content={this.state.user_id.toString()}
+              codeStyle="square"
+              size={100}
+            />
+            {/* ) : null} */}
+          </TouchableOpacity>
+          <View style={styles.secondContainer}>
+        
+            <Image
+              source={require("@images/people.jpg")}
+              style={{ width: 100, height: 100, borderRadius: 50 }}
+            />
+            <Text style={styles.text}>Dr. Maung Maung</Text>
+            <Text style={styles.text}>Doctor</Text>
+            <Text style={styles.text}>Department 1</Text>
+            <Text style={styles.text}>09 458518277</Text>
+            <Text style={styles.text}>7/khtthaka(N)111111</Text>
+            <Text
+              style={[styles.text, { height: 80, textAlignVertical: "top" }]}
+            >
+              Naypyitaw
+            </Text>
+          </View>
+       
+        </ScrollView>
+
+        <QRCodeModalView
+          // userID={this.state.user_id}
+          isOpen={this.state.isOpenQRCodeModal}
+          onClose={() => this.setState({ isOpenQRCodeModal: false })}
+        />
       </View>
     );
   }
@@ -48,6 +86,14 @@ const styles = StyleSheet.create({
     margin: 20,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor:"#039BE7",
+    padding:10,
+    borderWidth:1,
+    borderRadius:10,
+    borderColor:"#039BE7",
+    elevation:2,
+    paddingBottom:35,
+    paddingTop:20
   },
   thirdContainer: {
     flexDirection: "row",
@@ -64,6 +110,13 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderColor: "#ffffff",
     backgroundColor: "#ffffff",
-    elevation: 2,
+    elevation:5,
+  },
+  qrcodeBox: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    marginTop: 10,
+    marginRight:15
   },
 });
